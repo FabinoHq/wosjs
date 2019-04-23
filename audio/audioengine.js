bbf10bfa47ffcfb8e261a4d22289ad3a827776ee
FabinoHq <fabino.hq@gmail.com>
2019-04-23 12:38:17 +0200

    Add audio engine management

---
+++ audio/audioengine.js
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////
+//   _______                               ________________________________   //
+//   \\ .   \                     ________/ . . . . . . . . . . . . . .   /   //
+//    \\ .   \     ____       ___/ . . . . .   __________________________/    //
+//     \\ .   \   //   \   __/. . .  _________/   /    // .  _________/       //
+//      \\ .   \_//     \_//     ___/.  _____    /    // .  /_____            //
+//       \\ .   \/   _   \/    _/// .  /    \\   |    \\  .       \           //
+//        \\ .      /\\       /  || .  |    ||   |     \\______    \          //
+//         \\ .    /  \\     /   || .  \____//   |  _________//    /          //
+//          \\ .  /    \\   /    //  .           / // . . . .     /           //
+//           \\__/      \\_/    //______________/ //_____________/            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+//   This is free and unencumbered software released into the public domain.  //
+//                                                                            //
+//   Anyone is free to copy, modify, publish, use, compile, sell, or          //
+//   distribute this software, either in source code form or as a compiled    //
+//   binary, for any purpose, commercial or non-commercial, and by any        //
+//   means.                                                                   //
+//                                                                            //
+//   In jurisdictions that recognize copyright laws, the author or authors    //
+//   of this software dedicate any and all copyright interest in the          //
+//   software to the public domain. We make this dedication for the benefit   //
+//   of the public at large and to the detriment of our heirs and             //
+//   successors. We intend this dedication to be an overt act of              //
+//   relinquishment in perpetuity of all present and future rights to this    //
+//   software under copyright law.                                            //
+//                                                                            //
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          //
+//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF       //
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   //
+//   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR        //
+//   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,    //
+//   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR    //
+//   OTHER DEALINGS IN THE SOFTWARE.                                          //
+//                                                                            //
+//   For more information, please refer to <http://unlicense.org>             //
+////////////////////////////////////////////////////////////////////////////////
+//    WOS : Web Operating System                                              //
+//      audioengine.js : WOS Audio engine management                          //
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+//  AudioEngine class definition                                              //
+////////////////////////////////////////////////////////////////////////////////
+function AudioEngine(glPointer)
+{
+    // Audio engine loaded status
+    this.loaded = false;
+
+    // Audio engine context
+    this.context = null;
+}
+
+AudioEngine.prototype = {
+    ////////////////////////////////////////////////////////////////////////////
+    //  init : Init audio engine                                              //
+    ////////////////////////////////////////////////////////////////////////////
+    init: function()
+    {
+        this.loaded = false;
+
+        try {
+        this.context = new (window.AudioContext || window.webKitAudioContext)();
+        } catch(e) {}
+
+        // Check context
+        if (!this.context)
+        {
+            return false;
+        }
+
+        // Audio engine successfully loaded
+        this.loaded = true;
+        return true;
+    }
+};
+
+
