d73c06e29b5818cfaeb305efa6762f523c975dcf
FabinoHq <fabino.hq@gmail.com>
2019-04-24 14:00:59 +0200

    Change comment header

--- renderer/procsprite.js
+++ renderer/procsprite.js
@@ -37,7 +37,7 @@
 //   For more information, please refer to <http://unlicense.org>             //
 ////////////////////////////////////////////////////////////////////////////////
 //    WOS : Web Operating System                                              //
-//      renderer/procsprite.js : WOS Procedural sprite management             //
+//      renderer/procsprite.js : Procedural sprite management                 //
 ////////////////////////////////////////////////////////////////////////////////
 
 
